ae383a333575c3eed2039cf20b02f00b9f608b72
lovelin0523 <18356201582@163.com>
2023-12-26 20:54:13 +0800

    新增导出Editify组件

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './icon/iconfont.css'
 //引入国际化
 import i18n from './locale'
 //版本号
-const version = '0.1.2'
+const version = '0.1.3'
 //安装函数
 const install = (app, props) => {
 	const locale = (props ? props.locale : 'zh_CN') || 'zh_CN'
@@ -21,4 +21,4 @@ const stdin_default = {
 	version
 }
 
-export { stdin_default as default, install, version, AlexElement }
+export { stdin_default as default, install, version, Editify, AlexElement }
