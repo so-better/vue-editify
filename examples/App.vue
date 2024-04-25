b2997de0c7437697ee3187cd84207df16dd3c630
lovelin0523 <18356201582@163.com>
2024-04-25 10:12:59 +0800

    更新

--- examples/App.vue
+++ examples/App.vue
@@ -1,6 +1,6 @@
 <template>
 	<div style="padding: 10px; height: 100%; box-sizing: border-box">
-		<Editify ref="editify" border v-model="val" :menu="menuConfig" style="height: 100%" placeholder="Please Enter Text..." locale="zh_CN" :customTextPaste="customTextPaste" show-word-length></Editify>
+		<Editify ref="editify" border v-model="val" :menu="menuConfig" style="height: 100%" placeholder="Please Enter Text..." locale="zh_CN"></Editify>
 	</div>
 </template>
 <script setup lang="ts">
@@ -12,69 +12,13 @@ const editify = ref<InstanceType<typeof Editify> | null>(null)
 const menuConfig = ref<MenuConfigType>({
 	use: true,
 	mode: 'inner',
-	sequence: {
-		imgList: 101
-	},
-	image: {
-		accept: ['png'],
-		handleError: (error, file) => {
-			console.log(error, file)
-		}
-	},
 	sourceView: {
 		show: true
 	},
 	fullScreen: {
 		show: true
-	},
-	extends: {
-		imgList: {
-			title: '自定义菜单按钮',
-			leftBorder: true,
-			rightBorder: false,
-			disabled: false,
-			active: false,
-			type: 'select',
-			options: [
-				{
-					label: '自定义功能1',
-					value: '1',
-					style: {
-						color: '#f30'
-					}
-				},
-				{
-					label: '自定义功能2',
-					value: '2',
-					style: {
-						fontWeight: 'bold'
-					}
-				},
-				{
-					label: '自定义功能3',
-					value: '3'
-				}
-			],
-			onOperate: function (name, val, instance) {
-				console.log(name, val, instance)
-			},
-			default: () => h('span', {}, '自定义菜单')
-		}
 	}
 })
-const customTextPaste = function (data: string) {
-	const text = new AlexElement(
-		'text',
-		null,
-		null,
-		{
-			color: 'red',
-			'font-weight': 'bold'
-		},
-		data
-	)
-	editify.value!.editor!.insertElement(text)
-}
 </script>
 <style lang="less">
 html,
