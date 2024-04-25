e79ae6fe999b98f846c1f6fdf7ef8a28a7d39f9f
lovelin0523 <18356201582@163.com>
2024-04-25 21:18:01 +0800

    更新

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,10 @@ import Editify from './editify/editify.vue'
 
 //导出类型
 export type { ButtonTypeType, ButtonOptionsItemType, ButtonSelectConfigType, ButtonDisplayConfigType } from './components/button/props'
-export type { MenuButtonType, MenuSelectButtonType, MenuDisplayButtonType, MenuImageButtonType, MenuVideoButtonType, MenuTableButtonType, MenuCustomButtonType, CodeBlockToolbarType, TextToolbarType, ToolbarConfigType, MenuSequenceType, MenuModeType, MenuConfigType } from './core/tool'
+export type { MenuButtonType, MenuSelectButtonType, MenuDisplayButtonType, MenuImageButtonType, MenuVideoButtonType, MenuTableButtonType, MenuCustomButtonType, CodeBlockToolbarType, TextToolbarType, ToolbarConfigType, MenuSequenceType, MenuModeType, MenuConfigType, EditifyPluginType, EditifyPluginResultType } from './core/tool'
 export type { InsertImageUploadErrorType } from './components/insertImage/props'
 export type { InsertVideoUploadErrorType } from './components/insertVideo/props'
+export type { AttachmentAcceptType, AttachmentOptionsType } from './plugins/attachment'
 
 //导出编辑器操作方法
 export { getParsedomElementByElement, getCurrentParsedomElement, elementIsInList, elementIsInTask, isList, isTask, hasPreInRange, isRangeInPre, hasQuoteInRange, isRangeInQuote, hasListInRange, isRangeInList, hasTaskInRange, isRangeInTask, hasLinkInRange, hasTableInRange, hasImageInRange, hasVideoInRange, queryTextStyle, queryTextMark, getRangeText, setIndentIncrease, setIndentDecrease, setQuote, setAlign, setList, setTask, setTextStyle, setTextMark, removeTextStyle, removeTextMark, setLineHeight, insertLink, insertImage, insertVideo, insertTable, insertCodeBlock } from './core/function'
@@ -25,3 +26,6 @@ export { AlexElement } from 'alex-editor'
 
 //导出组件和安装函数
 export { install as default, install, Editify, version }
+
+//导出插件
+export { attachment } from './plugins/attachment'
