c7cf19650e777501a695639e8b24f53922873d31
lovelin0523 <18356201582@163.com>
2024-04-30 08:58:47 +0800

    更新

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -5,11 +5,11 @@ export type { ButtonTypeType, ButtonOptionsItemType, ButtonSelectConfigType, But
 export type { InsertImageUploadErrorType } from './components/insertImage/props';
 export type { InsertVideoUploadErrorType } from './components/insertVideo/props';
 export type { MenuButtonType, MenuSelectButtonType, MenuDisplayButtonType, MenuImageButtonType, MenuVideoButtonType, MenuTableButtonType, MenuCustomButtonType, CodeBlockToolbarType, TextToolbarType, ToolbarConfigType, MenuSequenceType, MenuModeType, MenuExtendType, MenuConfigType, PluginType, PluginResultType } from './core/tool';
-export type { AttachmentOptionsType } from './plugins/attachment';
-export type { InsertAttachmentUploadErrorType } from './plugins/attachment/insertAttachment/props';
 export { getParsedomElementByElement, getCurrentParsedomElement, elementIsInList, elementIsInTask, isList, isTask, hasPreInRange, isRangeInPre, hasQuoteInRange, isRangeInQuote, hasListInRange, isRangeInList, hasTaskInRange, isRangeInTask, hasLinkInRange, hasTableInRange, hasImageInRange, hasVideoInRange, queryTextStyle, queryTextMark, getRangeText, setIndentIncrease, setIndentDecrease, setQuote, setAlign, setList, setTask, setTextStyle, setTextMark, removeTextStyle, removeTextMark, setLineHeight, insertLink, insertImage, insertVideo, insertTable, insertCodeBlock } from './core/function';
 declare const install: FunctionPlugin;
-declare const version = "0.1.36";
+declare const version = "0.1.37";
 export { AlexElement } from 'alex-editor';
+export type { AttachmentOptionsType } from './plugins/attachment';
+export type { InsertAttachmentUploadErrorType } from './plugins/attachment/insertAttachment/props';
 export { attachment, isAttachment, hasAttachmentInRange } from './plugins/attachment';
 export { install as default, install, Editify, version };
--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,6 @@ export type { ButtonTypeType, ButtonOptionsItemType, ButtonSelectConfigType, But
 export type { InsertImageUploadErrorType } from './components/insertImage/props'
 export type { InsertVideoUploadErrorType } from './components/insertVideo/props'
 export type { MenuButtonType, MenuSelectButtonType, MenuDisplayButtonType, MenuImageButtonType, MenuVideoButtonType, MenuTableButtonType, MenuCustomButtonType, CodeBlockToolbarType, TextToolbarType, ToolbarConfigType, MenuSequenceType, MenuModeType, MenuExtendType, MenuConfigType, PluginType, PluginResultType } from './core/tool'
-//导出插件相关类型
-export type { AttachmentOptionsType } from './plugins/attachment'
-export type { InsertAttachmentUploadErrorType } from './plugins/attachment/insertAttachment/props'
 
 //导出编辑器操作方法
 export { getParsedomElementByElement, getCurrentParsedomElement, elementIsInList, elementIsInTask, isList, isTask, hasPreInRange, isRangeInPre, hasQuoteInRange, isRangeInQuote, hasListInRange, isRangeInList, hasTaskInRange, isRangeInTask, hasLinkInRange, hasTableInRange, hasImageInRange, hasVideoInRange, queryTextStyle, queryTextMark, getRangeText, setIndentIncrease, setIndentDecrease, setQuote, setAlign, setList, setTask, setTextStyle, setTextMark, removeTextStyle, removeTextMark, setLineHeight, insertLink, insertImage, insertVideo, insertTable, insertCodeBlock } from './core/function'
@@ -21,12 +18,14 @@ const install: FunctionPlugin = (app: App) => {
 	app.component(Editify.name!, Editify)
 }
 //版本号
-const version = '0.1.36'
+const version = '0.1.37'
 
 //导出AlexElement元素
 export { AlexElement } from 'alex-editor'
 
-//导出插件和插件相关方法
+//导出attachment插件相关的方法和类型
+export type { AttachmentOptionsType } from './plugins/attachment'
+export type { InsertAttachmentUploadErrorType } from './plugins/attachment/insertAttachment/props'
 export { attachment, isAttachment, hasAttachmentInRange } from './plugins/attachment'
 
 //导出组件和安装函数
