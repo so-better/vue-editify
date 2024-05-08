741048c30a5f561c0b1fa805cc10eade6a2dfb3c
lovelin0523 <18356201582@163.com>
2024-05-08 18:06:02 +0800

    更新

--- src/core/function.ts
+++ src/core/function.ts
@@ -1380,10 +1380,6 @@ export const insertSeparator = (editor: AlexEditor) => {
 	}
 	const separator = new AlexElement('closed', 'hr', null, null, null)
 	editor.insertElement(separator)
-	const leftSpace = AlexElement.getSpaceElement()
-	const rightSpace = AlexElement.getSpaceElement()
-	editor.addElementAfter(rightSpace, separator)
-	editor.addElementBefore(leftSpace, separator)
-	editor.range.anchor.moveToEnd(rightSpace)
-	editor.range.focus.moveToEnd(rightSpace)
+	editor.range.anchor.moveToEnd(separator)
+	editor.range.focus.moveToEnd(separator)
 }
