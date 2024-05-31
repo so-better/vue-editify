34538b377431283499f9c7de68836796134f348b
ling0523 <18356201582@163.com>
2024-05-31 08:23:07 +0800

    更新

--- examples/App.vue
+++ examples/App.vue
@@ -1,51 +1,12 @@
 <template>
 	<div style="padding: 10px; height: 100%; box-sizing: border-box">
-		<table>
-			<tbody>
-				<tr>
-					<td><br /></td>
-					<td><br /></td>
-					<td><br /></td>
-					<td><br /></td>
-					<td><br /></td>
-				</tr>
-				<tr>
-					<td><br /></td>
-					<td><br /></td>
-					<td><br /></td>
-					<td><br /></td>
-					<td><br /></td>
-				</tr>
-				<tr>
-					<td><br /></td>
-					<td><br /></td>
-					<td><br /></td>
-					<td><br /></td>
-					<td><br /></td>
-				</tr>
-				<tr>
-					<td><br /></td>
-					<td><br /></td>
-					<td><br /></td>
-					<td><br /></td>
-					<td><br /></td>
-				</tr>
-				<tr>
-					<td><br /></td>
-					<td><br /></td>
-					<td><br /></td>
-					<td><br /></td>
-					<td><br /></td>
-				</tr>
-			</tbody>
-		</table>
 		<Editify ref="editify" border v-model="val" :menu="menuConfig" style="height: 100%" placeholder="Please Enter Text..." :toolbar="toolbarConfig" locale="zh_CN" allow-paste-html :plugins="plugins" @rangeupdate="rangeupdate"></Editify>
 	</div>
 </template>
 <script setup lang="ts">
 import { h, ref } from 'vue'
 import { AlexElement, MenuConfigType, Editify, attachment, PluginType, mathformula, ToolbarConfigType, getMatchElementsByRange } from '../src/index'
-const val = ref<string>('<img src="https://www.ling0523.cn/images/image_1_1701871044699.jpg" /><table><tbody><tr><td><br></td><td><br></td><td><br></td><td><br></td><td><br></td></tr><tr><td><div>333</div></td><td><br></td><td><br></td><td><br></td><td><br></td></tr><tr><td><br></td><td><br></td><td><br></td><td><br></td><td><br></td></tr><tr><td><br></td><td><br></td><td><br></td><td><br></td><td><br></td></tr><tr><td><br></td><td><br></td><td><br></td><td><br></td><td><br></td></tr></tbody></table>')
+const val = ref<string>('<pre><br></pre><img src="https://www.ling0523.cn/images/image_1_1701871044699.jpg" /><table><tbody><tr><td><br></td><td><br></td><td><br></td><td><br></td><td><br></td></tr><tr><td><div>333</div></td><td><br></td><td><br></td><td><br></td><td><br></td></tr><tr><td><br></td><td><br></td><td><br></td><td><br></td><td><br></td></tr><tr><td><br></td><td><br></td><td><br></td><td><br></td><td><br></td></tr><tr><td><br></td><td><br></td><td><br></td><td><br></td><td><br></td></tr></tbody></table>')
 
 const editify = ref<InstanceType<typeof Editify> | null>(null)
 const menuConfig = ref<MenuConfigType>({
