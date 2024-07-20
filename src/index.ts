d7dd0cc80a3d05f4dab301760ae40aa0ac1bd625
so-better <18356201582@163.com>
2024-07-20 11:47:25 +0800

    更新

--- src/index.ts
+++ src/index.ts
@@ -104,7 +104,7 @@ const install = (app: App) => {
 	app.component(Editify.name!, Editify)
 }
 //版本号
-const version = '0.2.19'
+const version = '0.2.18'
 
 //导出组件和安装函数
 export { Editify as default, Editify, install, AlexElement, version }
